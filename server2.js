6e9dad3116e15ac9886c0651e32638ef5fff1cb4
VinlaRose <vinla.files@gmail.com>
2023-07-18 12:58:17 +0530

    commit

---
+++ server2.js
@@ -0,0 +1,30 @@
+// const express = require('express');
+// const axios = require('axios');
+
+// const app = express();
+// const PORT = 5000; // You can change this to any available port
+
+// app.use(express.json());
+
+// app.post('/generate-token', async (req, res) => {
+//   const apiKey = '4NKQ3-815C2-8T5Q2-16318-55301';
+//   const url = 'https://devcore02.cimet.io/v1/generate-token';
+
+//   try {
+//     const response = await axios.post(url, req.body, {
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Api-Key': apiKey,
+//       },
+//     });
+
+//     res.json(response.data);
+//   } catch (error) {
+//     console.error('Error calling the API:', error.message);
+//     res.status(500).json({ error: 'An error occurred while calling the API' });
+//   }
+// });
+
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+import { useEffect } from 'react';
+import './App.css';
+
+
+function App() {
+
+
+  return (
+
+    
+    <div className="App">
+
+      <div className="singleProductContainer">
+
+        <div className="top">
+          <div className="topLeft">
+            <image src = " " alt =" "/>
+            <div className="topLeftName">LogoName</div>
+          </div>
+          <div className="topMiddle">
+
+          </div>
+          <div className="topRight"></div>
+        </div>
+        <div className="middle"></div>
+        <div className="bottom"></div>
+
+      </div>
+      
+
+
+     
+    </div>
+  );
+}
+
+export default App;
